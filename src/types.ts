d9382a3a1136f31c5ad85e380a3dcfda5241f8c6
Paulo <pvpmartins@hotmail.com>
2022-12-12 18:30:45 -0300

    new types

--- src/types.ts
+++ src/types.ts
@@ -17,10 +17,43 @@ export interface InstallFormBuilderData {
   domain: string;
 }
 
-export interface Data {
+export interface CreateApiKeyData {
   name: string;
   description: string;
   permissions: Permission[];
+}
+
+export interface Permission {
+  name: string;
+}
+
+export interface CreateApiKeyOutput {
+  data: CreateApiKeyOutputData;
+}
+
+export interface CreateApiKeyOutputData {
+  security: Security;
+}
+
+export interface Security {
+  apiKey: APIKey;
+  __typename: string;
+}
+
+export interface APIKey {
+  data: APIKeyData;
+  error?: string;
+  __typename: string;
+}
+
+export interface APIKeyData {
+  id: string;
+  name: string;
+  description: string;
+  token: string;
+  permissions: Permission[];
+  createdOn: Date;
+  __typename: string;
 }
 
 export interface Permission {
